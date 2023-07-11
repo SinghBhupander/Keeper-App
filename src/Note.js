69e7b34351d6f462e5bba039984a52e0c0301e4f
Bhupander Singh Rathore <107210468+SinghBhupander@users.noreply.github.com>
2023-07-11 17:06:43 +0530

    Initial commit

---
+++ src/Note.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is title</h1>
+      <p> this is content</p>
+    </div>
+  );
+}
+export default Note;
